acf77362f03388d879ddd10db1896b8cb5b06b97
terehinairina <irisha.freyman@mail.ru>
2022-04-06 10:45:52 +0300

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int p = 1; p <= 10; p++) {
+            for (int o = 1; o <= 10; o++) {
+                System.out.println(p + " x " + o + " = " + p * o);
+            }
+            System.out.println("");
+        }
+    }
+}
